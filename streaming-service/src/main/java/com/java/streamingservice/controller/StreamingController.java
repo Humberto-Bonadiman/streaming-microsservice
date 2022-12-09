50bfff3c5db6bd19b2263a599975e31e7c57ccb3
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-12-09 10:44:00 -0400

    feat: finish to create controller

--- streaming-service/src/main/java/com/java/streamingservice/controller/StreamingController.java
+++ streaming-service/src/main/java/com/java/streamingservice/controller/StreamingController.java
@@ -1,24 +1,58 @@
 package com.java.streamingservice.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.java.streamingservice.dto.StreamingDto;
+import com.java.streamingservice.model.Streaming;
+import com.java.streamingservice.service.StreamingService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 // import io.swagger.v3.oas.annotations.Operation;
 
 @CrossOrigin
 @RestController
-@RequestMapping("streaming")
-public class StreamingController {
+@Tag(name = "Movies endpoint")
+public class StreamingController implements StreamingControllerInterface {
+
+	@Autowired
+	StreamingService streamingService;
+
+	@Override
+	@Operation(summary = "Create streaming data")
+	public ResponseEntity<Streaming> create(StreamingDto streamingDto) {
+    	return ResponseEntity.status(HttpStatus.CREATED).body(streamingService.create(streamingDto));
+    }
+
+	@Override
+	@Operation(summary = "Show all streaming data")
+	public ResponseEntity<List<Streaming>> findAll() {
+		return ResponseEntity.status(HttpStatus.OK).body(streamingService.showAll());
+	}
+
+	@Override
+	@Operation(summary = "Find a streaming data by id")
+	public ResponseEntity<Streaming> findById(String id) {
+		return ResponseEntity.status(HttpStatus.OK).body(streamingService.findById(id));
+	}
 
 	/**
-	 * 	@Operation(summary = "Create movie data")
-	public ResponseEntity<Movies> create(MoviesDto moviesDto) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(moviesService.create(moviesDto));
+	 * 	@Override
+	public ResponseEntity<Streaming> update(String id, StreamingDto streamingDto) {
+		return ResponseEntity.status(HttpStatus.OK).body(streamingService)
 	}
 	 */
 
-	
+	@Override
+	@Operation(summary = "Delete a streaming data by id")
+	public ResponseEntity<Object> deleteById(String id) {
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 }
---
+++ streaming-service/src/main/java/com/java/streamingservice/controller/StreamingControllerInterface.java
@@ -0,0 +1,65 @@
+package com.java.streamingservice.controller;
+
+import com.java.streamingservice.dto.StreamingDto;
+import com.java.streamingservice.model.Streaming;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/streaming")
+public interface StreamingControllerInterface {
+
+	  @PostMapping
+	  @ApiResponses(value = {
+	      @ApiResponse(responseCode = "201", description = "Create a streaming data", 
+	          content = { @Content(mediaType = "application/json", 
+	          schema = @Schema(implementation = Streaming.class)) }),
+	      @ApiResponse(responseCode = "400", description = "Wrong format",
+	          content = @Content)})
+	  public ResponseEntity<Streaming> create(@RequestBody StreamingDto streamingDto);
+
+	  @GetMapping
+	  @ApiResponses(value = {
+	      @ApiResponse(responseCode = "200", description = "Show all streaming data", 
+	          content = { @Content(mediaType = "application/json", 
+	          array = @ArraySchema(schema = @Schema(implementation = Streaming.class))) })})
+	  public ResponseEntity<List<Streaming>> findAll();
+
+	  @GetMapping("/{id}")
+	  @ApiResponses(value = {
+		      @ApiResponse(responseCode = "200", description = "Show a streaming by id",
+		          content = { @Content(mediaType = "application/json", 
+		    	  schema = @Schema(implementation = Streaming.class)) }),
+		      @ApiResponse(responseCode = "404", description = "Streaming not found",
+		          content = @Content)})
+      public ResponseEntity<Streaming> findById(@PathVariable String id);
+
+	  /**
+	   * 	  @PatchMapping("/{id}")
+	  @ApiResponses(value = {
+		      @ApiResponse(responseCode = "200", description = "Update a streaming",
+		          content = { @Content(mediaType = "application/json", 
+		    	  schema = @Schema(implementation = Streaming.class)) }),
+		      @ApiResponse(responseCode = "404", description = "Streaming not found",
+		          content = @Content)})
+	  public ResponseEntity<Streaming> update(@PathVariable String id, @RequestBody StreamingDto streamingDto);
+	   */
+
+	  @DeleteMapping("/{id}")
+	  @ApiResponses(value = {
+	      @ApiResponse(responseCode = "204", content = @Content),
+	      @ApiResponse(responseCode = "404", description = "Streaming not found",
+	          content = @Content)})
+	  public ResponseEntity<Object> deleteById(@PathVariable String id);
+}
